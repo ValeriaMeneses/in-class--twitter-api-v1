6162205b4a0d9e77fc3ffee67c9d6358f40d2e37
valeria <valeriairaismeneses@gmail.com>
2017-11-21 16:49:23 -0600

    part 7 authentication

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const bodyParser = require('body-parser');
 
 const pageRouter = require('./src/routes/pageRouter.js');
 const apiRouter = require('./src/routes/apiRouter.js');
+const authRouter = require('./src/routes/authRouter.js');
 
 const connectToDb = require('./src/database/dbConnect.js');
 const dbConfigObj = require('./knexfile.js');
+
+// Import the libraries + middleware
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+
+const registerLocalStrategy = require('./src/middleware/passport-local--registerLocalStrategy.js');
+const { configDeserializeUser, configSerializeUser } = require('./src/helpers/passport-local--sessionActions.js');
 
 const app = express();
 
@@ -22,15 +31,32 @@ app.engine('ejs', ejs.renderFile);
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/src/views`);
 
-//
+//// JSON parse configuration
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//cookieParser, cookieSession configuration
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'cookiemonster',
+  secret: 'superduperhipersecret',
+  httpOnly: true,
+  signed: false
+}));
+
+//passport middleware configuration
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(registerLocalStrategy());
+passport.serializeUser(configSerializeUser());
+passport.deserializeUser(configDeserializeUser());
 
 // Static files
 app.use(express.static(`${__dirname}/public`))
 
 app.use('/', pageRouter);
 app.use('/api/v1', apiRouter);
+app.use('/auth', authRouter);
 
 // Create 404 route
 app.use((req, res) => {
