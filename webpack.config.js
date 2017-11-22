60aefa31a86daca885f99bec612002a6eaafcb38
valeria <valeriairaismeneses@gmail.com>
2017-11-22 16:37:51 -0600

    part-8-react

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './src/client/js/App.js',
+    devtool: 'cheap-eval-source-map',
+    output: {
+      path: `${__dirname}/public/js`,
+      filename: 'bundle.js'
+    },
+    resolve: {
+    extensions: ['.js', '.json']
+    },
+    stats: {
+    colors: true,
+    reasons: true
+    },
+    module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+    }
+};
